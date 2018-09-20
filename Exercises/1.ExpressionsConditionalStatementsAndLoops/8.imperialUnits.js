6672f7a90ee126f11bf47d6ee59a60da22b976b5
plamen1982 <plamen.hristov@ymail.com>
2018-09-20 20:06:21 +0900

    Imperial Units - 1.8

--- Exercises/1.ExpressionsConditionalStatementsAndLoops/8.imperialUnits.js
+++ Exercises/1.ExpressionsConditionalStatementsAndLoops/8.imperialUnits.js
@@ -1,19 +1,8 @@
 function solve(inches) {
-    if(inches > 12) {
         let feets = Math.floor(inches / 12);
         let remainingInches = inches % 12;
         console.log(`${feets}'-${remainingInches}"`);
-    }
-
-    else if(inches < 0) {
-        console.log(`0'-0"`);
-    }
-
-    else {
-        console.log(`0'-${inches}"`);
-    }
 }
 
 solve(36);
 solve(55);
-solve(-10);
