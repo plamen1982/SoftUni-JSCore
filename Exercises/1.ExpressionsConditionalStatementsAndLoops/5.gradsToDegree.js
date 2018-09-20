05f98012a484948dfa85f7a14802a1ff1b0e71b6
plamen1982 <plamen.hristov@ymail.com>
2018-09-20 18:55:13 +0900

    Grads to Degree - 1.5

---
+++ Exercises/1.ExpressionsConditionalStatementsAndLoops/5.gradsToDegree.js
@@ -0,0 +1,14 @@
+function solve(grads) {
+
+    let degrees = (grads / 10) * 9;
+    let degreesInRange = (degrees % 360);
+    if(degreesInRange < 0) {
+        degreesInRange += 360;
+    }
+    console.log(degreesInRange);
+}
+
+// solve(100);
+// solve(400);
+// solve(850);
+solve(-50);
