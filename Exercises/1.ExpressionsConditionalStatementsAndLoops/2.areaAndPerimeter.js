6fd9cad5891c0db8fdb3d5c3c33e4b4b54f93a6d
plamen1982 <plamen.hristov@ymail.com>
2018-09-20 18:29:23 +0900

    Area and Perimeter 1.2

---
+++ Exercises/1.ExpressionsConditionalStatementsAndLoops/2.areaAndPerimeter.js
@@ -0,0 +1,11 @@
+function solve(a, b) {
+    let area = a*b;
+    let perimeter = 2*a + 2*b;
+    console.log(area);
+    console.log(perimeter);
+}
+
+
+solve(2, 2);
+solve(1, 3);
+solve(2.5, 3.14);
