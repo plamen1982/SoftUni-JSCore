fa4b3fd0362722c0ab9aefb35bd656f22d8d37bd
plamen1982 <plamen.hristov@ymail.com>
2018-09-22 14:48:55 +0900

    Biggest of 3 Numbers - 1.14

---
+++ Exercises/1.ExpressionsConditionalStatementsAndLoops/14.biggestOf3Numbers.js
@@ -0,0 +1,9 @@
+function solve([num1, num2, num3]) {
+    console.log(Math.max(num1, num2, num3));
+}
+
+solve([ 5, -2, 7 ]);
+solve([ 130, 5, 99]);
+solve([ 43, 43.2, 43.1 ]);
+solve([ 5, 5, 5 ]);
+solve([ -10, -20, -30 ]);
