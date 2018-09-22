cb9373ff8e0964901a763c22c41869fe06d6f809
plamen1982 <plamen.hristov@ymail.com>
2018-09-22 14:27:17 +0900

    Assign properties - 1.12

---
+++ Exercises/1.ExpressionsConditionalStatementsAndLoops/12.assignProperties.js
@@ -0,0 +1,10 @@
+function solve([key1, value1, key2, value2, key3, value3]) {
+    let keyValueObject = {};
+    keyValueObject[key1] = value1;
+    keyValueObject[key2] = value2;
+    keyValueObject[key3] = value3;
+    console.log(keyValueObject)
+}
+
+// solve(['name', 'Pesho', 'age', '23', 'gender', 'male']);
+solve(['ssid', '90127461', 'status', 'admin', 'expires', '600']);
