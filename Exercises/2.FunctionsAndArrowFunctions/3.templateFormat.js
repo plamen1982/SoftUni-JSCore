1325da0d4bd3f3ee5f02104a4b491748a3f2459b
plamen1982 <plamen.hristov@ymail.com>
2018-09-27 18:30:43 +0900

    Road Radar - 2.3

---
+++ Exercises/2.FunctionsAndArrowFunctions/3.templateFormat.js
@@ -0,0 +1,33 @@
+function templateFormat(quiz) {
+    debugger;
+    function printQuestionAnswer(question, answer) {
+        return `
+    <question>  
+        ${question} 
+    </question> 
+    <answer>  
+        ${answer} 
+    </answer>`   
+    }
+
+    function printHeader() {
+        return `<?xml version="1.0" encoding="UTF-8"?>
+<quiz>`
+    }
+
+    function printFooter() {
+        return `
+</quiz>`
+    }
+    let header = printHeader();
+    let body = '';
+    let footer = printFooter();
+
+    for(let i = 0; i < quiz.length; i += 2) {
+        body += printQuestionAnswer(quiz[i], quiz[i + 1]);
+    }
+
+    return header + body + footer;
+}
+
+console.log(templateFormat(["Who was the forty-second president of the U.S.A.?","William Jefferson Clinton"]));
