b87b8f4e6317b2df534851ae88814be05eac4aa7
plamen1982 <plamen.hristov@ymail.com>
2018-09-29 20:54:32 +0900

    added new lines for readability

--- Exercises/3.Arrays-Lab/1.sumFirstLast.js
+++ Exercises/3.Arrays-Lab/1.sumFirstLast.js
@@ -1,5 +1,5 @@
 function sumFirstLast(array) {
-    return (+(array[0]) + +(array[array.length - 1]));
+    return (Number(array[0]) + Number(array[array.length - 1]));
 }
 
 console.log(sumFirstLast(['20', '30', '40']));
--- Exercises/3.Arrays-Lab/4.firstAndLastKNumbers.js
+++ Exercises/3.Arrays-Lab/4.firstAndLastKNumbers.js
@@ -1,8 +1,12 @@
 function firstAndLastKNumbers(array) {
     let [ numbersElements ] = array;
     let copyArray = array.slice(1);
-    console.log(copyArray.slice(0, numbersElements).join(" "));
-    console.log(copyArray.slice((copyArray.length - numbersElements), copyArray.length).join(" "));
+    console.log(copyArray
+            .slice(0, numbersElements)
+            .join(" "));
+    console.log(copyArray
+            .slice((copyArray.length - numbersElements), copyArray.length)
+            .join(" "));
 }
 
 firstAndLastKNumbers([2, 7, 8, 9]);
