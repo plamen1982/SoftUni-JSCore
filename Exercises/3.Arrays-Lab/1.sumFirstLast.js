456ad3e434ad4d5138aa701a97805d3ff66d7260
plamen1982 <plamen.hristov@ymail.com>
2018-09-29 20:14:48 +0900

    refactoring sum first last

--- Exercises/3.Arrays-Lab/1.sumFirstLast.js
+++ Exercises/3.Arrays-Lab/1.sumFirstLast.js
@@ -1,22 +1,6 @@
 function sumFirstLast(array) {
-    let localArray = [].concat(array);
-    // let localArray = array.slice(0);
-    localArray = localArray.map(x => Number(x));
-    let sum = localArray.reduce((accum, current, currentIndex, array) => {
-        //if array has only one member doubled it
-        if(array.length === 1) {
-            return 2 * (accum + current)
-            //accumulate the first element
-        } else if(currentIndex === 0) {
-            return accum + current; 
-            //accumulate the last element
-        } else if(currentIndex === array.length - 1) {
-            return accum + current;
-        }
-        return accum;
-    }, 0)
-    return sum;
+    return (+(array[0]) + +(array[array.length - 1]));
 }
 
-// console.log(sumFirstLast(['20', '30', '40']));
-console.log(sumFirstLast(['20']));
+console.log(sumFirstLast(['20', '30', '40']));
+// console.log(sumFirstLast(['20']));
