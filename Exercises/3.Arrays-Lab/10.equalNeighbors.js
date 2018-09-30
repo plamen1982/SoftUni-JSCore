784efe26b6e13e8f3b130ab3fab6034b3595ae9f
plamen1982 <plamen.hristov@ymail.com>
2018-09-30 10:07:20 +0900

    Equal Neighbors

---
+++ Exercises/3.Arrays-Lab/10.equalNeighbors.js
@@ -0,0 +1,29 @@
+function equalNeighbors(matrix) {
+    let counter = 0;
+    //check horizonal neighbors
+    for(let i = 0; i < matrix.length; i++) {
+        for(let j = 0; j < matrix[i].length - 1; j++) {
+            if(matrix[i][j] === matrix[i][j + 1]) {
+                counter++;
+            }
+        }
+    }
+
+    //check the vertical neighbors
+    for(let i = 0; i < matrix.length - 1; i++) {
+        for(let j = 0; j < matrix[i].length; j++) {
+            if(matrix[i][j] === matrix[i + 1][j]) {
+                counter++;
+            }
+        }
+    }
+    return counter;
+}
+
+let result = equalNeighbors([
+    ['5', '5', '4'],
+    ['5', '0', '5'],
+    ['2', '5', '5'],
+
+]);
+console.log(result);
