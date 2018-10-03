9ea3b630a60a5a8711ce351c0918c5c00aa4c122
plamen1982 <plamen.hristov@ymail.com>
2018-10-03 17:26:29 +0900

    Starts With - 4.3

---
+++ Exercises/4.StringsAndRegex/3.ifStartsWithSubstr.js
@@ -0,0 +1,6 @@
+function isStartsWithSubstr(theString, substr) {
+    console.log(theString.startsWith(substr));
+}
+
+// isStartsWithSubstr('How have you been?', 'how');
+isStartsWithSubstr('The quick brown fox…', 'The quick brown fox…');
