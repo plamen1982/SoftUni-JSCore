68264e28e55d046823b46989e47bd0f2a6e73a92
plamen1982 <plamen.hristov@ymail.com>
2018-10-04 19:50:33 +0900

    Refactir 4.8

--- Exercises/4.StringsAndRegex/8.findOccuranceOfWord.js
+++ Exercises/4.StringsAndRegex/8.findOccuranceOfWord.js
@@ -1,6 +1,7 @@
 function findOccuranceOfWord(theString, searchedWord) {
     let regex = new RegExp(`\\b${searchedWord}\\b`, 'gi');
-    return theString.match(regex).filter((element) => element !== '').length;
+    let result = theString.match(regex);
+        return result ? result.length : 0;
 }
 
 console.log(findOccuranceOfWord('There was one. Therefore I bought it. I wouldn’t buy it otherwise.', 
