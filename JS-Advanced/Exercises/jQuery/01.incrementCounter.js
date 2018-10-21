479cb2ba7578d4a9a73e1cde8f893402a0c15c49
plamen1982 <plamen.hristov@ymail.com>
2018-10-21 11:49:50 +0900

    timer - jQuery - 2

--- JS-Advanced/Exercises/jQuery/01.incrementCounter.js
+++ JS-Advanced/Exercises/jQuery/01.incrementCounter.js
@@ -1,8 +1,7 @@
-/// <reference path="../typings/globals/jquery/index.d.ts" />
+<reference path="globals/jquery/index.d.ts" />
 
 function increment (selector) {
     $selector = $(selector);
-
     $selector
         .append($('<textarea>').addClass('counter').attr('disabled', true).val('0'))
         .append($('<button>').addClass('btn').attr('id', 'incrementBtn').text('Increment').on('click', incrementValue))
@@ -10,8 +9,7 @@ function increment (selector) {
         .append($('<ul>').addClass('results'));
 
         function incrementValue() {
-            console.log($selector.first())
-            $selector.children(0).val(+$('.counter').val() + 1);
+            $('.counter').val(+$('.counter').val() + 1);
         }
 
         function addToList() {
---
+++ JS-Advanced/Exercises/jQuery/02.timer.js
@@ -0,0 +1,33 @@
+// <reference path="globals/jquery/index.d.ts" />
+// <span id="hours" class="timer">00</span>:
+// <span id="minutes" class="timer">00</span>:
+// <span id="seconds" class="timer">00</span>
+// <button id="start-timer">Start</button>
+// <button id="stop-timer">Stop</button>
+function timer() {
+    let isRunning = false;
+    let intervalId = false;
+    let time = 0;
+
+    $('#start-timer').on('click', () => {
+        if(!isRunning) {
+            intervalId = setInterval(startTimer, 1000);
+            isRunning = true;
+        }
+    });
+
+    $('#stop-timer').on('click', () => {
+        if(isRunning) {
+            clearInterval(intervalId);
+            isRunning = false;
+        }
+    })
+
+    function startTimer() {
+        time += 1;
+        $('#hours').text(('0' + parseInt(time / 3600)).slice(-2));
+        $('#minutes').text(('0' + (parseInt(time / 60) % 60)).slice(-2));
+        $('#seconds').text(('0' + parseInt(time % 60)).slice(-2));
+    }
+
+}
