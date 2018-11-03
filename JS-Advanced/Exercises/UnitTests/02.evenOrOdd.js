8d2b6c48e888efb5539caefd354c0788eb6eaf7f
plamen1982 <plamen.hristov@ymail.com>
2018-11-03 04:06:55 -0100

    evenOrOdd Unit Test - 2

--- JS-Advanced/Exercises/UnitTests/02.evenOrOdd.js
+++
@@ -1,7 +0,0 @@
-let expect = require('chai').expect;
-let assert = require('chai').assert;
-describe("sum(arr)", function() {
-    it("should return 3 for [1, 2]", function() {
-        expect(3).equal(3)
-    })
-})
---
+++ JS-Advanced/Exercises/UnitTests/tests/02.evenOrOdd.js
@@ -0,0 +1,26 @@
+let expect = require('chai').expect;
+let assert = require('chai').assert;
+
+function isOddOrEven(string) {
+    if (typeof (string) !== 'string') {
+        return undefined;
+    }
+    if (string.length % 2 === 0) {
+        return "even";
+    }
+
+    return "odd";
+}
+describe('isOddOrEven(string)', () => {
+
+    it('should return odd when the length of the number is odd', () => {
+        assert.equal(isOddOrEven("5"), "odd");
+    });
+    it('should return even when the length of the number is even', () => {
+        assert.equal(isOddOrEven("43"), "even");
+    });
+    it('should return undefined when note a type string is passed', () => {
+        assert.equal(isOddOrEven(5), undefined);
+    });
+
+});
