4a89077d44a62594cc545dc9bd731b16385ff2a8
“[plamen1982]” <plamen.hristov@ymail.com>
2018-12-09 16:00:58 +0200

    Registration post connected with kinvey

--- JS-Applications/Exercises/Routing/Team-Manager-Skeleton/scripts/app.js
+++ JS-Applications/Exercises/Routing/Team-Manager-Skeleton/scripts/app.js
@@ -3,23 +3,30 @@ $(() => {
         // TODO: Define all the routes
         this.use('Handlebars', 'hbs');
         this.route('get', '/index.html', displayHome);
-        this.route('get', '#/home', displayHome);;
+        this.route('get', '#/home', displayHome);
         this.route('get', '#/about', displayAbout);
+
         this.route('get', '#/login', displayLogin);
+        this.route('post', '#/login', sendLogin);
+
         this.route('get', '#/register', displayRegister);
+        this.route('post', '#/register', sendRegister);
+
 
         function displayHome(context) {
 
-            context.loggedId = !!(sessionStorage.getItem('authtoke'));
+            context.loggedIn = !!(sessionStorage.getItem('authtoken'));
             context.username = sessionStorage.getItem('username');
             context.teamId = !!(sessionStorage.getItem('teamId'));
 
-            this.loadPartials({
+            context.loadPartials({
                 header: './templates/common/header.hbs',
                 footer: './templates/common/footer.hbs',
             }).then(function(context) {
                 this.partial('./templates/home/home.hbs');
-            }).catch(auth.handleError);
+            }).catch(function(error) {
+                auth.handleError(error)
+            });
         }
 
         function displayAbout(context) {
@@ -51,6 +58,10 @@ $(() => {
             }).catch(auth.handleError);
         }
 
+        function sendLogin(context) {
+            auth.post()
+        }
+
         function displayRegister(context) {
 
             context.loggedId = !!(sessionStorage.getItem('authtoke'));
@@ -64,6 +75,20 @@ $(() => {
             }).then(function(context) {
                 this.partial('./templates/register/registerPage.hbs');
             }).catch(auth.handleError);
+        }
+
+        function sendRegister(context) {
+            let username = context.params.username;
+            let password = context.params.password;
+            let repeatPassword = context.params.repeatPassword;
+
+            password !== repeatPassword ? auth.showError('The given passwords doesn\'t matched') : auth.register(username, password).then(function(userInfo) {
+                auth.saveSession(userInfo);
+                auth.showInfo('The registration is successful')
+                displayHome(context);
+            }).catch(function(error) {
+                auth.handleError(error)
+            });
         }
     });
     app.run();
