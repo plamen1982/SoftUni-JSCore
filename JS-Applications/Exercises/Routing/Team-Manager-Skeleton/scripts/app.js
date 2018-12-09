4fbe33a7013164b7c27669d61f0d77ccc8cf1f2b
“[plamen1982]” <plamen.hristov@ymail.com>
2018-12-09 14:24:28 +0200

    home and about routes connected

--- JS-Applications/Exercises/Routing/Team-Manager-Skeleton/scripts/app.js
+++ JS-Applications/Exercises/Routing/Team-Manager-Skeleton/scripts/app.js
@@ -2,9 +2,37 @@ $(() => {
     const app = Sammy('#main', function () {
         // TODO: Define all the routes
         this.use('Handlebars', 'hbs');
-        this.route('get', '#/index.html', () => {
-            this.swap('<h2>Hello</h2>');
-        })
+        this.route('get', '/index.html', displayHome);
+        this.route('get', '#/home', displayHome);;
+        this.route('get', '#/about', displayAbout);
+
+        function displayHome(context) {
+
+            context.loggedId = !!(sessionStorage.getItem('authtoke'));
+            context.username = sessionStorage.getItem('username');
+            context.teamId = !!(sessionStorage.getItem('teamId'));
+
+            this.loadPartials({
+                header: './templates/common/header.hbs',
+                footer: './templates/common/footer.hbs',
+            }).then(function(context) {
+                this.partial('./templates/home/home.hbs');
+            }).catch(auth.handleError);
+        }
+
+        function displayAbout(context) {
+
+            context.loggedId = !!(sessionStorage.getItem('authtoke'));
+            context.username = sessionStorage.getItem('username');
+            context.teamId = !!(sessionStorage.getItem('teamId'));
+
+            this.loadPartials({
+                header: './templates/common/header.hbs',
+                footer: './templates/common/footer.hbs',
+            }).then(function(context) {
+                this.partial('./templates/about/about.hbs');
+            }).catch(auth.handleError);
+        }
     });
     app.run();
 });
